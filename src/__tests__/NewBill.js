4529c62afff0cb562545329f57590d94eecbad72
Sayaka Vincent <sfv2010@hotmail.fr>
2023-06-15 14:12:11 +0200

    Fix Post integration test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,12 +5,15 @@ import "@testing-library/jest-dom";
 import { screen, waitFor, fireEvent } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
-import { ROUTES } from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
 
 //Mock the module to use the mockStore module
 jest.mock("../app/store", () => mockStore);
+const onNavigate = (pathname) => {
+    document.body.innerHTML = ROUTES({ pathname });
+};
 
 describe("Given I am connected as an employee", () => {
     beforeEach(() => {
@@ -56,7 +59,6 @@ describe("Given I am connected as an employee", () => {
             const handleChangeFile = jest.fn(initNewBills.handleChangeFile);
             file.addEventListener("change", handleChangeFile);
             //The File() constructor creates a new File object instance.
-
             const testFile = new File(["testFile"], "test.jpg", {
                 type: "image/jpg",
             });
@@ -71,11 +73,8 @@ describe("Given I am connected as an employee", () => {
     });
 
     describe("when I am on NewBill Page and I click the submit button", () => {
-        test("Then, handleSubmit function should be called", () => {
+        test("Then, handleSubmit function should be called", async () => {
             document.body.innerHTML = NewBillUI();
-            const onNavigate = (pathname) => {
-                document.body.innerHTML = ROUTES({ pathname });
-            };
             const initNewBills = new NewBill({
                 document,
                 onNavigate,
@@ -83,130 +82,97 @@ describe("Given I am connected as an employee", () => {
                 localStorage: window.localStorage,
             });
 
-            const formNewBill = screen.getByTestId("form-new-bill");
+            const submit = screen.getByTestId("form-new-bill");
             const handleSubmit = jest.fn(initNewBills.handleSubmit);
-            formNewBill.addEventListener("submit", handleSubmit);
-            fireEvent.submit(formNewBill);
+            submit.addEventListener("submit", handleSubmit);
+            fireEvent.submit(submit);
             expect(handleSubmit).toHaveBeenCalled();
         });
     });
-});
-
-//Post integration test
-describe("Given I am connected as an employee", () => {
-    describe("When I create a new bill", () => {
-        test("fetches NewBill from mock API POST", async () => {
-            Object.defineProperty(window, "localStorage", {
-                value: localStorageMock,
-            });
-            window.localStorage.setItem(
-                "user",
-                JSON.stringify({
-                    type: "Employee",
-                    email: "a@a",
-                })
-            );
+    //Post integration test
+    describe("When I am on NewBill Page and I submit a valid bill form", () => {
+        test("then a bill is created", async () => {
             document.body.innerHTML = NewBillUI();
-            const onNavigate = (pathname) => {
-                document.body.innerHTML = ROUTES({ pathname });
-            };
-
-            // For tracking mocked API methods
-            const createSpy = jest.spyOn(mockStore.bills(), "create");
-            // const createSpy = jest
-            //     .spyOn(mockStore.bills(), "create")
-            //     .mockResolvedValue({
-            //         fileUrl: "https://localhost:3456/images/test.jpg",
-            //         key: "1234",
-            //     });
-            const updateSpy = jest.spyOn(mockStore.bills(), "update");
-            console.log("createSpy", createSpy);
-
-            expect(jest.spyOn(mockStore.bills(), "create")).toBe(createSpy);
-            const initNewBill = new NewBill({
+            const initNewBills = new NewBill({
                 document,
                 onNavigate,
                 store: mockStore,
                 localStorage: window.localStorage,
             });
+            const imgFile = new File(["test.txt"], "test.txt", {
+                type: "text/txt",
+            });
+            const inputFile = screen.getByTestId("file");
+            const handleChangeFile = jest.fn((e) =>
+                initNewBills.handleChangeFile(e)
+            );
+            inputFile.addEventListener("change", handleChangeFile);
+            fireEvent.change(inputFile, { target: { files: [imgFile] } });
 
-            // Form input data
-            const billData = {
+            const createBill = jest.fn(mockStore.bills().create);
+            const updateBill = jest.fn(mockStore.bills().update);
+            const { fileUrl, key } = await createBill();
+
+            expect(createBill).toHaveBeenCalledTimes(1);
+            expect(key).toBe("1234");
+            expect(fileUrl).toBe("https://localhost:3456/images/test.jpg");
+
+            const newBillUpdate = await updateBill();
+            expect(updateBill).toHaveBeenCalledTimes(1);
+
+            expect(newBillUpdate).toEqual({
+                id: "47qAXb6fIm2zOKkLzMro",
+                vat: "80",
+                fileUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+                status: "pending",
                 type: "Hôtel et logement",
-                name: "Test Bill",
-                amount: 100,
-                date: "2023-06-01",
-                vat: "20",
+                commentary: "séminaire billed",
+                name: "encore",
+                fileName: "preview-facture-free-201801-pdf-1.jpg",
+                date: "2004-04-04",
+                amount: 400,
+                commentAdmin: "ok",
+                email: "a@a",
                 pct: 20,
-                commentary: "Test comment",
-                fileUrl: "../img/hotel.png",
-                fileName: "hotel.png",
-                file: new File(["testFile"], "test.jpg", {
-                    type: "image/jpg",
-                }),
-            };
-
-            // // Get form element and set value
-            const typeInput = screen.getByTestId("expense-type");
-            fireEvent.change(typeInput, { target: { value: billData.type } });
-            expect(typeInput.value).toBe(billData.type);
-
-            const nameInput = screen.getByTestId("expense-name");
-            fireEvent.change(nameInput, { target: { value: billData.name } });
-
-            const amountInput = screen.getByTestId("amount");
-            fireEvent.change(amountInput, {
-                target: { value: billData.amount },
             });
+            let searchURL = ROUTES_PATH.Bills;
+            searchURL = searchURL.slice(-5);
+            expect(searchURL).toBe("bills");
+        });
+    });
 
-            const dateInput = screen.getByTestId("datepicker");
-            fireEvent.change(dateInput, { target: { value: billData.date } });
-
-            const vatInput = screen.getByTestId("vat");
-            fireEvent.change(vatInput, { target: { value: billData.vat } });
-
-            const pctInput = screen.getByTestId("pct");
-            fireEvent.change(pctInput, { target: { value: billData.pct } });
-
-            const commentaryInput = screen.getByTestId("commentary");
-            fireEvent.change(commentaryInput, {
-                target: { value: billData.commentary },
+    describe("When I am on NewBill Page and I submit a incomplete bill form", () => {
+        test("then a message required field is displayed", async () => {
+            document.body.innerHTML = NewBillUI();
+            const initNewBills = new NewBill({
+                document,
+                onNavigate,
+                store: mockStore,
+                localStorage: window.localStorage,
             });
-            initNewBill.fileUrl = billData.fileUrl;
-            initNewBill.fileName = billData.fileName;
+            const submit = screen.getByTestId("form-new-bill");
 
-            const sendExpenseText = await waitFor(() =>
-                screen.getByText("Envoyer une note de frais")
-            );
-            expect(sendExpenseText).toBeTruthy();
-
-            // Mock file upload processing
-            const file = screen.getByTestId("file");
-            // const testFile = new File(["testFile"], "test.jpg", {
-            //     type: "image/jpg",
-            // });
-            fireEvent.change(file, {
-                target: {
-                    files: [billData.file],
-                },
-            });
-            console.log("file", file.files[0]);
-            console.log("testFile", billData.file.name);
-            expect(file.files[0]).toBe(billData.file);
-
-            const formNewBill = screen.getByTestId("form-new-bill");
-            fireEvent.submit(formNewBill);
-            // Wait for form submission and Check that updateSpy was called
-            //await waitFor(() => expect(createSpy).toHaveBeenCalled());
-            await waitFor(() => expect(updateSpy).toHaveBeenCalled());
-
-            // Check if the expected form data was passed to the API method
-
-            // Check that the screen transitions after submitting the form
-            const billHeaderText = await waitFor(() =>
-                screen.getByText("Mes notes de frais")
-            );
-            expect(billHeaderText).toBeTruthy();
+            const handleSubmit = jest.fn((e) => initNewBills.handleSubmit(e));
+            initNewBills.createBill = (initNewBills) => initNewBills;
+            document.querySelector(`input[data-testid="expense-name"]`).value =
+                "";
+            document.querySelector(`input[data-testid="datepicker"]`).value =
+                "";
+            document.querySelector(`select[data-testid="expense-type"]`).value =
+                "";
+            document.querySelector(`input[data-testid="amount"]`).value = "";
+            document.querySelector(`input[data-testid="vat"]`).value = "";
+            document.querySelector(`input[data-testid="pct"]`).value = "";
+            document.querySelector(`textarea[data-testid="commentary"]`).value =
+                "";
+            initNewBills.fileUrl = "";
+            initNewBills.fileName = "";
+            submit.addEventListener("click", handleSubmit);
+            fireEvent.click(submit);
+            let searchURL = ROUTES_PATH.NewBill;
+            searchURL = searchURL.slice(-3);
+            expect(searchURL).toBe("new");
         });
     });
 });
